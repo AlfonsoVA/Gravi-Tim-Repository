78d3350b7a34320ca2a5a5b6acb5b9a7e7af4474
AlfonsoVA <fonsi2010@live.com.mx>
2020-10-13 22:32:09 -0500

    Doors added

---
+++ Gravi-Tim project/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DoorScript : MonoBehaviour{    
+
+    private GameObject chipCounter;
+    private TextMeshProUGUI txtCount;
+    
+
+    void Start(){
+        chipCounter = GameObject.FindWithTag("ChipCounter");        
+        txtCount = chipCounter.GetComponent<TMPro.TextMeshProUGUI>();
+    }    
+
+    void Update(){}
+
+    private void OnCollisionEnter2D(Collision2D col){           
+        if((col.gameObject.tag == "Player") && (int.Parse(txtCount.text) > 0)){               
+            int counter = (int.Parse(txtCount.text)) - 1;
+            txtCount.text = counter.ToString();
+            Object.Destroy(gameObject);     
+        }
+    }
+}
